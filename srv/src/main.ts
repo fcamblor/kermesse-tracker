a8782836b9b7b131b69bf8a10106d983d3f9d49a
fcamblor <fcamblor@gmail.com>
2022-05-14 23:48:21 +0200

    configured cors for localhost domain

--- srv/src/main.ts
+++ srv/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(bodyParser.json({limit: '5mb'}));
+  app.enableCors({
+    origin: [ /localhost/ ],
+    methods: ['DELETE', 'GET', 'PUT', 'POST', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Content-Length', 'Authorization']
+  });
   await app.listen(3000);
 }
 bootstrap();
