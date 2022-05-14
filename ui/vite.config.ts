de41f486c0351af340159baf4f9b50752a10c0c5
fcamblor <fcamblor@gmail.com>
2022-05-14 19:25:13 +0200

    fixed build for prod

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -2,13 +2,9 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0'
+  },
   build: {
-    lib: {
-      entry: 'src/my-element.ts',
-      formats: ['es']
-    },
-    rollupOptions: {
-      external: /^lit/
-    }
   }
 })
