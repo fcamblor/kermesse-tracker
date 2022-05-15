7decef6a50e7696fe708328e4b06ad1f484b2393
fcamblor <fcamblor@gmail.com>
2022-05-15 11:47:01 +0200

    introduced Db utility component allowing to access postgres DB

--- srv/src/app.module.ts
+++ srv/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {Db} from "./persistance/db";
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+      AppService,
+      Db
+  ],
 })
 export class AppModule {}
