80296a9273d1ec3abc8c456591ccac796fb69032
fcamblor <fcamblor@gmail.com>
2022-05-19 00:45:23 +0200

    added setting button on home screen

--- ui/src/views/kt-home.view.ts
+++ ui/src/views/kt-home.view.ts
@@ -65,6 +65,7 @@ export class KTHomeView extends LitElement {
         .members="${this.families.flatMap(familyMembers)}"
         @on-member-selected="${(e: CustomEvent<MemberSelected>) => this.showFamilyCheckin(e.detail.member)}"
     ></people-selector>
+    <button type="button" class="btn btn-primary mx-2" @click="${() => Router.navigateToSettingsPage()}">Configuration</button>
         `
     }
 
